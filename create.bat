9f4b77773acfdd1b6615baa623ce63b2424aab76
Teodosi Kachakov <teo.kachakov@gmail.com>
2021-07-05 01:50:06 +0300

    Updated git remote add origin
    Clearer syntax

--- create.bat
+++ create.bat
@@ -1,7 +1,7 @@
 @python C:\Users\sample.user\my_python_scripts\repo_create.py %*
 @cd "C:\Users\sample.user\somefolder\projects\my_projects\%1"
 @git init
-@git remote add origin https://github.com/yourUsername>1/%1.git
+@git remote add origin https://github.com/yourUsername/%1.git
 @echo.> README.md
 @git add .
 @git commit -m "initial commit"
